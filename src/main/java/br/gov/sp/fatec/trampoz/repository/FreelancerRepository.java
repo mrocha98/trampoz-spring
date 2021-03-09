56a7faf9bb9662eb37fa4b1e13cbca89dbb1f631
Arthur Reis <arthurreis758@gmail.com>
2021-03-08 22:48:47 -0300

    Desenvolvimento

--- src/main/java/br/gov/sp/fatec/trampoz/repository/FreelancerRepository.java
+++ src/main/java/br/gov/sp/fatec/trampoz/repository/FreelancerRepository.java
@@ -4,8 +4,12 @@ import br.gov.sp.fatec.trampoz.entity.Freelancer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface FreelancerRepository extends JpaRepository<Freelancer, UUID> {
+
+    public Freelancer findByName(String name);
+
 }
