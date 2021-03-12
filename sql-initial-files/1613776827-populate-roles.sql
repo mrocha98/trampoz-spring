10419935e1aa246e0d4221060ed39f2903c96e78
Matheus Rocha <matheus_rocha98@outlook.com>
2021-03-12 01:42:01 -0300

    fix role COMPANY in sql initial file

--- sql-initial-files/1613776827-populate-roles.sql
+++ sql-initial-files/1613776827-populate-roles.sql
@@ -3,7 +3,7 @@ BEGIN;
 INSERT INTO rol_roles (rol_id, rol_name)
 VALUES (gen_random_uuid(), 'ADMIN'),
        (gen_random_uuid(), 'FREELANCER'),
-       (gen_random_uuid(), 'CONTRACTOR')
+       (gen_random_uuid(), 'COMPANY')
 RETURNING *;
 
 COMMIT;
