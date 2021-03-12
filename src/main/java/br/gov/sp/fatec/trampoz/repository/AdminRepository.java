72c6e97ce9c588302b127e6ffd5ecfbe300eb38a
Matheus Rocha <matheus_rocha98@outlook.com>
2021-03-12 01:55:50 -0300

    create AdminRepository & AdminService

--- src/main/java/br/gov/sp/fatec/trampoz/repository/AdminRepository.java
+++ src/main/java/br/gov/sp/fatec/trampoz/repository/AdminRepository.java
@@ -4,8 +4,10 @@ import br.gov.sp.fatec.trampoz.entity.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, UUID> {
+    List<Admin> findByEmail(String email);
 }
